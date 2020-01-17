d9aa2950b60cb74ec1de6df727cba69809917690
aooiuu <aooiu@qq.com>
2020-01-17 14:05:11 +0800

    清理文件

---
+++ src/router/async-routes.js
@@ -0,0 +1,8 @@
+/**
+ * asyncRoutes
+ * the routes that need to be dynamically loaded based on user roles
+ */
+export const asyncRoutes = [
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
