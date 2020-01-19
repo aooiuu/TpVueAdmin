e501de6f3214c268b92a14afdd92a73c97dd0d3b
aooiuu <aooiu@qq.com>
2020-01-19 14:23:38 +0800

    add admin/auth_group/roletree

--- application/admin/controller/AuthGroup.php
+++ application/admin/controller/AuthGroup.php
@@ -53,12 +53,52 @@ class AuthGroup extends Base
         if (!isset($id)) {
             $this->result([], 2, '缺少参数');
         }
-
         // 取出所有菜单
         $AuthRule = collection(AuthRule::select())->toArray();
-        // 取出所有可选菜单
-
-        // 获取已分配的菜单
-        $this->result($AuthRule, 0, '操作成功');
+        // 已选菜单
+        $authGroup = $this->model->get($id);
+        $resultRules = []; // 可选菜单
+        $parentAuthGroup = [];
+        if ($authGroup->pid) {
+            $parentAuthGroup = $this->model->get($authGroup->pid);
+        } else {
+            $parentAuthGroup = $authGroup;
+        }
+        // --
+        $rulesArr = explode(',', $parentAuthGroup->rules);
+        if (in_array('*', $rulesArr)) {
+            // 返回全部菜单
+            $resultRules = $AuthRule;
+        } else {
+            foreach ($AuthRule as $k => $v) {
+                if (in_array($v['id'], $rulesArr)) {
+                    $resultRules[] = $v;
+                }
+            }
+        }
+        // 兼容
+        foreach ($resultRules as $k => $v) {
+            $resultRules[$k]['state'] = [
+                'selected' => false,
+            ];
+        }
+        // 已选菜单
+        $rulesArr = explode(',', $authGroup->rules);
+        if (in_array('*', $rulesArr)) {
+            foreach ($resultRules as $k => $v) {
+                $resultRules[$k]['state'] = [
+                    'selected' => true,
+                ];
+            }
+        } else {
+            foreach ($resultRules as $k => $v) {
+                if (in_array($v['id'], $rulesArr)) {
+                    $resultRules[$k]['state'] = [
+                        'selected' => true,
+                    ];
+                }
+            }
+        }
+        $this->result($resultRules, 0, 'success');
     }
 }
