bfa79e332d2dc8021f8a411d3ef8e6deb47d93da
aooiuu <aooiu@qq.com>
2020-02-10 17:48:19 +0800

    chore: 构建生产版本去除log

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+const plugins = []
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console')
+}
+  module.exports = {
+  plugins,
   presets: [
     '@vue/app'
   ]
