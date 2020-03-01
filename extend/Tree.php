cab381367342ee8f7386ec804519fe0c591923ef
aooiuu <aooiu@qq.com>
2020-03-01 17:18:32 +0800

    feat: extend

---
+++ extend/Tree.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * 参考 fast\Tree
+ */
+
+class Tree
+{
+
+    protected static $instance;
+
+    /**
+     * 生成树型结构所需要的2维数组
+     * @var array
+     */
+    public $arr = [];
+    public $pidname = 'pid';
+    public $nbsp = "&nbsp;";
+
+    /**
+     * 初始化
+     * @access public
+     * @return Tree
+     */
+    public static function instance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * 初始化方法
+     * @param array  $arr     2维数组，例如：
+     *      array(
+     *      1 => array('id'=>'1','pid'=>0,'name'=>'一级栏目一'),
+     *      2 => array('id'=>'2','pid'=>0,'name'=>'一级栏目二'),
+     *      3 => array('id'=>'3','pid'=>1,'name'=>'二级栏目一'),
+     *      4 => array('id'=>'4','pid'=>1,'name'=>'二级栏目二'),
+     *      5 => array('id'=>'5','pid'=>2,'name'=>'二级栏目三'),
+     *      6 => array('id'=>'6','pid'=>3,'name'=>'三级栏目一'),
+     *      7 => array('id'=>'7','pid'=>3,'name'=>'三级栏目二')
+     *      )
+     * @param string $pidname 父字段名称
+     * @param string $nbsp    空格占位符
+     * @return Tree
+     */
+    public function init($arr = [], $pidname = null, $nbsp = null)
+    {
+        $this->arr = $arr;
+        if (!is_null($pidname)) {
+            $this->pidname = $pidname;
+        }
+        if (!is_null($nbsp)) {
+            $this->nbsp = $nbsp;
+        }
+        return $this;
+    }
+
+    /**
+     * 得到子级数组
+     * @param int
+     * @return array
+     */
+    public function getChild($myid)
+    {
+        $newarr = [];
+        foreach ($this->arr as $value) {
+            if (!isset($value['id'])) {
+                continue;
+            }
+            if ($value[$this->pidname] == $myid) {
+                $newarr[$value['id']] = $value;
+            }
+        }
+        return $newarr;
+    }
+
+    /**
+     * 读取指定节点的所有孩子节点
+     * @param int     $myid     节点ID
+     * @param boolean $withself 是否包含自身
+     * @return array
+     */
+    public function getChildren($myid, $withself = false)
+    {
+        $newarr = [];
+        foreach ($this->arr as $value) {
+            if (!isset($value['id'])) {
+                continue;
+            }
+            if ($value[$this->pidname] == $myid) {
+                $newarr[] = $value;
+                $newarr = array_merge($newarr, $this->getChildren($value['id']));
+            } elseif ($withself && $value['id'] == $myid) {
+                $newarr[] = $value;
+            }
+        }
+        return $newarr;
+    }
+
+    /**
+     * 读取指定节点的所有孩子节点ID
+     * @param int     $myid     节点ID
+     * @param boolean $withself 是否包含自身
+     * @return array
+     */
+    public function getChildrenIds($myid, $withself = false)
+    {
+        $childrenlist = $this->getChildren($myid, $withself);
+        $childrenids = [];
+        foreach ($childrenlist as $k => $v) {
+            $childrenids[] = $v['id'];
+        }
+        return $childrenids;
+    }
+
+    /**
+     * 得到当前位置父辈数组
+     * @param int
+     * @return array
+     */
+    public function getParent($myid)
+    {
+        $pid = 0;
+        $newarr = [];
+        foreach ($this->arr as $value) {
+            if (!isset($value['id'])) {
+                continue;
+            }
+            if ($value['id'] == $myid) {
+                $pid = $value[$this->pidname];
+                break;
+            }
+        }
+        if ($pid) {
+            foreach ($this->arr as $value) {
+                if ($value['id'] == $pid) {
+                    $newarr[] = $value;
+                    break;
+                }
+            }
+        }
+        return $newarr;
+    }
+
+    /**
+     * 得到当前位置所有父辈数组
+     * @param int
+     * @param bool $withself 是否包含自己
+     * @return array
+     */
+    public function getParents($myid, $withself = false)
+    {
+        $pid = 0;
+        $newarr = [];
+        foreach ($this->arr as $value) {
+            if (!isset($value['id'])) {
+                continue;
+            }
+            if ($value['id'] == $myid) {
+                if ($withself) {
+                    $newarr[] = $value;
+                }
+                $pid = $value[$this->pidname];
+                break;
+            }
+        }
+        if ($pid) {
+            $arr = $this->getParents($pid, true);
+            $newarr = array_merge($arr, $newarr);
+        }
+        return $newarr;
+    }
+
+    /**
+     * 读取指定节点所有父类节点ID
+     * @param int     $myid
+     * @param boolean $withself
+     * @return array
+     */
+    public function getParentsIds($myid, $withself = false)
+    {
+        $parentlist = $this->getParents($myid, $withself);
+        $parentsids = [];
+        foreach ($parentlist as $k => $v) {
+            $parentsids[] = $v['id'];
+        }
+        return $parentsids;
+    }
+}
