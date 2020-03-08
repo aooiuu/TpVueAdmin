59a542684a76e7b7f9d1b16dba807d8182cc67cb
aooiuu <aooiu@qq.com>
2020-03-08 22:53:06 +0800

    feat: 添加 trait Backend

---
+++ application/admin/library/Backend.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * CURD
+ */
+
+namespace app\admin\library;
+
+trait Backend
+{
+  /**
+   * 查看
+   */
+  public function index()
+  {
+    $this->result([], 0);
+  }
+  /**
+   * 回收站
+   */
+  public function recyclebin()
+  {
+    $this->result([], 0);
+  }
+  /**
+   * 添加
+   */
+  public function add()
+  {
+    $this->result([], 0);
+  }
+  /**
+   * 编辑
+   */
+  public function edit()
+  {
+    $this->result([], 0);
+  }
+  /**
+   * 删除
+   */
+  public function del()
+  {
+    $this->result([], 0);
+  }
+  /**
+   * 真实删除
+   */
+  public function destroy()
+  {
+    $this->result([], 0);
+  }
+  /**
+   * 还原
+   */
+  public function derestore()
+  {
+    $this->result([], 0);
+  }
+}
